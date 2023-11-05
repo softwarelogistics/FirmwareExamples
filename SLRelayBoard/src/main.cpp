24c2992bc91e4f89bf6c1cd6338307d201ef4f00
Kevin D. Wolf <kevinw@software-logistics.com>
2023-11-05 18:07:02 -0500

    refactor and cleanup app code

--- SLRelayBoard/src/main.cpp
+++ SLRelayBoard/src/main.cpp
@@ -1,11 +1,11 @@
-#define RELAY_BRD_V1
+#define PROD_BRD_V1
 
 #include <Arduino.h>
 #include <NuvIoT.h>
 
 #include <Wire.h>
 
-#define TEMP_SNSR_SKU "Relay Board"
+#define TEMP_SNSR_SKU "RLY010"
 #define FIRMWARE_VERSION "0.5.1"
 #define HARDWARE_REVISION "5.0"
 
@@ -13,12 +13,8 @@ byte buffer[8];
 bool running = true;
 
 void setup(){
-uint8_t new_mac[8] = {0x10, 0x02, 0x03, 0x04, 0x05, 0x06};
-esp_base_mac_addr_set(new_mac);
-
   delay(1000);
 
-  console.setVerboseLogging(true);
   configureConsole();
 
   state.init(TEMP_SNSR_SKU, FIRMWARE_VERSION, HARDWARE_REVISION, "pcl001", 010);
@@ -41,20 +37,16 @@ esp_base_mac_addr_set(new_mac);
   welcome(TEMP_SNSR_SKU, FIRMWARE_VERSION);
 
   String btName = "NuvIoT - " + (sysConfig.DeviceId == "" ? "Relay Board" : sysConfig.DeviceId);
-  console.println("BT DEVICE NAME: " + btName);
 
   BT.begin(btName.c_str(), "RELAY-005");
 
   wifiMgr.setup();  
+
+  configureModem();
+  connect();
+  console.println("setup=completed;");
 }
 
 void loop(){  
-  adc.loop(); 
-
-  console.setVerboseLogging(true);
-  adc.debugPrint();
-  wifiMgr.loop();
-  BT.update();
-  hal.loop();
-  delay(1000);
+  commonLoop();
 }
